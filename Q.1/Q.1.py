248d638f61c5cc5d1bf1e6e858d1cc9d216034e5
192525265simats-design <192525265.simats@saveetha.com>
2026-08-04 10:48:15 +0530

    Implement linear regression model for rent prediction
    This script uses linear regression to predict apartment rent based on floor area, number of rooms, and location.

---
+++ Q.1/Q.1.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+# Sample Dataset
+data = {
+    'FloorArea': [800, 1000, 1200, 1500, 1800],
+    'Rooms': [2, 3, 3, 4, 5],
+    'Location': [1, 2, 2, 3, 3],
+    'Rent': [15000, 20000, 25000, 30000, 35000]
+}
+
+df = pd.DataFrame(data)
+
+X = df[['FloorArea', 'Rooms', 'Location']]
+y = df['Rent']
+
+model = LinearRegression()
+model.fit(X, y)
+
+# Test Data
+area = 1400
+rooms = 4
+location = 3
+
+prediction = model.predict([[area, rooms, location]])
+
+print("Predicted Apartment Rent =", prediction[0])
